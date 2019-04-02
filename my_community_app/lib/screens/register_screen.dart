632df3f4d0c47e272dbf283d1066d5ef4c91e4b0
Andryut <aahuertasca@unal.edu.co>
2019-04-01 21:30:47 -0500

    Some things added

--- my_community_app/lib/screens/register_screen.dart
+++ my_community_app/lib/screens/register_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
-  LoginScreen({Key key, this.title}) : super(key: key);
+class RegisterScreen extends StatefulWidget {
+  RegisterScreen({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _RegisterScreenState createState() => _RegisterScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
