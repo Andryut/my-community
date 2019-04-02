9832523761968b774e0ab5f21c30ac77a0695f39
Santiago Velandia Casas <svelandiacasas@gmail.com>
2019-04-02 17:09:18 -0500

    navigation bar and settings screen created

---
+++ my_community_app/lib/screens/home/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'app_bodies/settings/settings_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int _selectedIndex = 2; //Navigation index selected
+  final _widgetOptions = [ //App bodies
+    Text('Index 0: Chats'),
+    Text('Index 1: Events'),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( // App main scaffold
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.question_answer, color: Colors.blue,), title: Text('Chat')),
+          BottomNavigationBarItem(icon: Icon(Icons.people, color: Colors.blue,), title: Text('Events')),
+          BottomNavigationBarItem(icon: Icon(Icons.settings, color: Colors.blue,), title: Text('Settings')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.indigo,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {//Change the app body
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
